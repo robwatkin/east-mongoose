0bf62f5160f039e54391d1de35170a6bb98cfb38
Rob Watkin <rob@letsbuild.com>
2020-12-21 21:50:11 +0100

    Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("east-mongoose");
+import path from "path";
+
+class Adapter {
+  params: {};
+
+  constructor(params: {}) {
+    this.params = params;
+  }
+
+  getTemplatePath() {
+    return path.join(__dirname, 'migrationTemplates', 'promises.js');
+  };
+
+
+}
